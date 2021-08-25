f1fc9a2a3440a7f3b2533e8bcaea0bfe2c54b055
wilmerG123 <garnicawilmer@gmail.com>
2021-08-24 22:06:19 -0500

    eliminacion carpeta netv

--- Netv/src/main/java/com/netv/repositorio/IRepoPlanes.java
+++
@@ -1,9 +0,0 @@
-package com.netv.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.netv.model.Planes;
-
-public interface IRepoPlanes extends JpaRepository<Planes, Long>{
-
-}
