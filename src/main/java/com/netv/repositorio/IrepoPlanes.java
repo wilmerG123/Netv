9de1ac61c0a12af77c38cbcfb0a5c372dedb67ec
wilmerG123 <garnicawilmer@gmail.com>
2021-08-24 19:44:08 -0500

    Primer commit subida del proyecto

---
+++ src/main/java/com/netv/repositorio/IrepoPlanes.java
@@ -0,0 +1,9 @@
+package com.netv.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.netv.model.Planes;
+
+public interface IrepoPlanes extends JpaRepository<Planes, Long>{
+
+}
